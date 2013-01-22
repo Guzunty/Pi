172141401e7450345c5b2d1c302f752df5350a2f
guzunty <guzunty@gmail.com>
2013-01-22 13:02:16 +0000

    add input read capability to gz_16i8o test program

--- src/gz_16i8o/gz_16i8o.c
+++ src/gz_16i8o/gz_16i8o.c
@@ -28,13 +28,45 @@
 #include <gz_spi.h>
 
 void exercise_outputs(unsigned char a, unsigned char b) {
-	  unsigned char output[2];
-	  output[0] = a;
-	  gz_spi_write(output);          // pass output bytes to SPI
-      sleep(1);
-      output[0] = b;
-	  gz_spi_write(output);          // pass output bytes to SPI
-	  sleep(1);
+  unsigned char output[2];
+  output[0] = a;
+  gz_spi_write(output);          // pass output bytes to SPI
+  usleep(100000);
+  output[0] = b;
+  gz_spi_write(output);          // pass output bytes to SPI
+  usleep(100000);
+}
+
+void display_inputs() {
+  int row, col;
+  unsigned char inputs[2];
+  getmaxyx(stdscr, row, col);		/* get the screen boundaries */
+  int center_x = col / 2;
+  int center_y = row / 2;
+  int start_y = center_y - 2;
+  int start_x = center_x - 16;
+  mvprintw(start_y, center_x - 8, "Input bit status");
+  start_y++;
+  mvprintw(start_y, start_x, "F E D C B A 9 8 7 6 5 4 3 2 1 0");
+  gz_spi_read(inputs);
+  unsigned char mask = 0x01;
+  int i = 0;
+  for (; i < 16; i++) {
+	  int cur_byte = 0;
+	  if (i > 7) {
+		  cur_byte = 1;
+	  }
+	  if (inputs[cur_byte] & mask) {
+		  mvprintw(center_y, start_x + 30 - (2 * i), "1");
+	  }
+	  else {
+		  mvprintw(center_y, start_x + 30 - (2 * i), "0");
+	  }
+	  mask = mask << 1;
+	  if (i == 7) {
+		  mask = 0x01;
+	  }
+  }
 }
 
 int main(int argc, char* argv[])
@@ -43,6 +75,7 @@ int main(int argc, char* argv[])
     nodelay(stdscr, 1);               // don't wait for key presses
     noecho();                         // don't echo key presses
     gz_spi_set_width(2);              // Pass blocks of 2 bytes on SPI
+    erase();
     printw("Toggling all outputs.\n");
 	printw("Press 'n' for next test, any other key to stop.\n");
 	int key = 0;
@@ -54,6 +87,7 @@ int main(int argc, char* argv[])
 	  }
 	}
 	if (key == 'n') {
+	  erase();
       printw("Toggling alternate outputs.\n");
 	  printw("Press 'n' for next test, any other key to stop.\n");
       while(1) {
@@ -65,6 +99,7 @@ int main(int argc, char* argv[])
 	  }
 	}
 	if (key == 'n') {
+	  erase();
       printw("Walking outputs.\n");
 	  printw("Press 'n' for next test, any other key to stop.\n");
 	  unsigned char current = 0xfe;
@@ -80,7 +115,22 @@ int main(int argc, char* argv[])
 	    }
 	  }
 	}
+	if (key == 'n') {
+	  erase();
+	  curs_set(0);                     // Hide the cursor
+      printw("Reading inputs.\n");
+	  printw("Press any key to stop.\n");
+      while(1) {
+		display_inputs();
+        key = getch();
+        if (key != -1) {
+		  break;
+	    }
+	  }
+	  curs_set(1);
+	}
     gz_spi_close();                   // close SPI channel
+	erase();
     reset_shell_mode();               // turn off ncurses
     return 0;
 }
