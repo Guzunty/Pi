c38e9fa34b4c28d5fe7ccd9fc4bbfab37550f34a
Derek Campbell <guzunty@gmail.com>
2015-01-29 19:51:44 +0000

    Update with rewritten core that has been radio tested.

--- src/gz_ppm/gz_ppm.vhd
+++ src/gz_ppm/gz_ppm.vhd
@@ -1,128 +1,163 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company:        Guzunty 
+-- Engineer:       CDM
 -- 
--- Create Date:    14:29:26 07/02/2013 
--- Design Name: 
--- Module Name:    gz_ppm - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
+-- Create Date:    15:23:47 01/20/2015 
+-- Design Name:    gz_ppm
+-- Module Name:    gz_ppm - Behavioral
+-- Project Name:   gz_ppm
+-- Target Devices: XC9572XL
+-- Tool versions:  ISE 14
+-- Description:    Decodes Pulse Position Modulation input to
+--                 up to eight servo signal outputs, plus pulse
+--                 width digitisation with an interrupt driven
+--                 SPI interface.
 -- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- 
+-- Revision 1.5
+-- Additional Comments: Requires Fitter setting - optimise for area.
+--                      Radio tested but not flight tested. Different
+--                      flight rigs may produce different noise
+--                      environments. You are strongly advised to fully
+--                      exercise this core in a static test rig before
+--                      flying it. Note that the pulse demultiplex
+--                      operation is dependent on a hardware clock from
+--                      the Raspberry Pi, so if that reboots, control
+--                      could be lost. You could always make your own
+--                      clock circuit if this is a concern, frequency
+--                      needs to be about 2MHz.
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity gz_ppm is
-    Port ( ppm_in : in  STD_LOGIC;
-	        sync : out STD_LOGIC;
-           ppm_int_req : out  STD_LOGIC;
-           ppm_clk : in  STD_LOGIC;
-           miso : out  STD_LOGIC;
-           sclk : in  STD_LOGIC;
-           sel : in  STD_LOGIC;
-			  pwms : out STD_LOGIC_VECTOR (7 downto 0) := (others => '0')
-	 );
+  Port ( ppm_i     : in  STD_LOGIC;
+         ppm_clk_i : in  STD_LOGIC;
+         sync_o    : out STD_LOGIC;
+         pwms_o    : out  STD_LOGIC_VECTOR (7 downto 0);
+         ppm_irq_o : out  STD_LOGIC := '0';
+         miso_o    : out  STD_LOGIC;
+         sclk_i    : in  STD_LOGIC;
+         sel_i     : in  STD_LOGIC);
 end gz_ppm;
 
 architecture Behavioral of gz_ppm is
-signal pulse_width_counter: unsigned(14 downto 0) := (others => '0');
-signal output_register: unsigned(15 downto 0) := (others => '1');
-signal inter_frame: std_logic := '0';
-signal interframe_overrun: std_logic := '0';
+  signal channel_counter : unsigned (3 downto 0) := (others => '0');
+  signal output_register: std_logic_vector(15 downto 0);
+  signal reset : std_logic := '0';
+  signal sync : std_logic := '0';
+  signal servo : std_logic;
+  signal irq_assert : std_logic;
+  signal irq_deassert : std_logic;
 begin
 
--- output_register(12) is the synchronized flag
-ppm_int_req <= (not inter_frame) and (not interframe_overrun)
-                           and output_register(12) and (not ppm_in);
-sync <= output_register(12);
+  servo <= ppm_i and sync and not reset;
+  pwms_o <= (0 => servo, others => '0') when channel_counter = 0 else
+            (1 => servo, others => '0') when channel_counter = 1 else
+            (2 => servo, others => '0') when channel_counter = 2 else
+            (3 => servo, others => '0') when channel_counter = 3 else
+            (4 => servo, others => '0') when channel_counter = 4 else
+            (5 => servo, others => '0') when channel_counter = 5 else
+            (6 => servo, others => '0') when channel_counter = 6 else
+            (7 => servo, others => '0') when channel_counter = 7 else
+            (others => '0');
+  sync_o <= sync;
 
-process(ppm_clk, ppm_in) is
-variable reset: std_logic := '0';
-begin
-  if (ppm_in = '0' and reset = '0') then
-    pulse_width_counter <= (others => '0');
-	 reset := '1';
-  elsif (rising_edge(ppm_clk)) then
-    if (ppm_in = '1') then
-      reset := '0';
-	 end if;
-    if (pulse_width_counter = 32767) then
-	   -- reset condition, or 16ms passed
-	   -- without a ppm pulse, signal is lost.
-	   output_register(12) <= '0'; -- signal synchronization lost
-    else
-	   if (pulse_width_counter > 4301) then
-        -- Detected inter frame gap (count of 4301 ~= 2.1 ms)
-	     output_register(12) <= '1'; -- signal synchronization achieved
-		  inter_frame <= '1';
-		else
-		  if (pulse_width_counter > 1023) then -- allow pulse to end (0.3ms)
-		    inter_frame <= '0';
-		  end if;
-		end if;
-      pulse_width_counter <= pulse_width_counter + 1;
-	 end if;
-  end if; -- pulse clock rising edge
-end process;
-
-process(ppm_in) is
-variable pulse_counter: unsigned(3 downto 0) := (others => '0');
-begin
-  if (falling_edge(ppm_in)) then
-    if (inter_frame = '1') then
-      pulse_counter := (others => '0');  -- reset channel counter
-		if (output_register(12) = '1') then
-		  pwms(0) <= '1';
-		end if;
-    else
-      -- increment channel count
-      if (pulse_counter /= 8) then
-		  if (output_register(12) = '1') then
-	       pwms(to_integer(pulse_counter)) <= '0';
-		  end if;
-		  interframe_overrun <= '0';
-        output_register(15 downto 13) <= pulse_counter(2 downto 0);
-        pulse_counter := pulse_counter + 1;
-		  if (output_register(12) = '1' and pulse_counter /= 8) then
-	       pwms(to_integer(pulse_counter)) <= '1';
-		  end if;
-      else
-		  interframe_overrun <= '1';
-        pulse_counter := (others => '0'); -- handle Fr Sky Receiver @ 18 ms frame time
+  process (ppm_clk_i)
+  variable pulse_width : unsigned (14 downto 0);
+  variable Q0, Q1, Q2 : std_logic;
+  variable ppm : std_logic := '0';
+  begin
+    if rising_edge(ppm_clk_i) then
+      Q2 := Q1;
+      Q1 := Q0;
+      Q0 := ppm_i;
+      if Q0 = '0' and Q1 = '0' and Q2 = '1' then
+        -- falling edge of ppm signal (debounced)
+        ppm := '0';
       end if;
-      -- load the current channel to output register
-      output_register(11 downto 0) <= pulse_width_counter(11 downto 0); 
-    end if;  -- in-frame pulse
-  end if; -- ppm_in falling edge
-end process;
-
-process (sclk, sel) is
-variable bit_cnt: natural range 0 to 15 := 15;
-begin
-  if (sel = '0') then
-    if (rising_edge(sclk)) then
-      miso <= output_register(bit_cnt);
-    end if;
-    if (falling_edge(sclk)) then
-      if (bit_cnt = 0) then
-        bit_cnt := 15;
+      if Q0 = '1' and Q1 = '1' and Q2 = '0' then
+        -- rising edge of ppm signal (debounced)
+        irq_deassert <= '1';
+        pulse_width := (others => '0');
+        ppm := '1';
       else
-        bit_cnt := bit_cnt - 1;
+        irq_deassert <= '0';
+      end if;
+      if pulse_width = 32767 then
+        sync <= '0';
+        if ppm = '0' then
+          pulse_width := (others => '0');
+        end if;
+      else
+        if ppm = '1' then
+          pulse_width := pulse_width + 1;
+          if pulse_width > 4100 then
+            reset <= '1';
+            sync <= '1';
+          else
+            reset <= '0';
+          end if;
+        end if;
+      end if;
+      if Q0 = '0' and Q1 = '0' and Q2 = '1' then
+        if reset = '1' then
+          channel_counter <= (others => '0');
+        else
+		    output_register(15) <= '0';            -- register bit not used
+          output_register(14 downto 12) <= std_logic_vector(channel_counter(2 downto 0));
+          output_register(11 downto 0) <= std_logic_vector(pulse_width(11 downto 0));
+          irq_assert <= '1';
+			 -- If we are on the 8th pulse, we are hitting the FrSky case where
+          -- the interframe pulse is indistinguishable from a servo pulse. By
+			 -- resetting the counter, we will continue to output pulses and
+			 -- produce accurate digitisations of the pulse widths so long as
+			 -- there are no glitch pulses. As soon as one of the channels de-
+			 -- saturates, synchronisation is restored within one frame in any
+			 -- case.
+          if channel_counter < 8 then
+            channel_counter <= channel_counter + 1;
+          else
+            channel_counter <= (others => '0');
+          end if;
+        end if;
+      else
+        irq_assert <= '0';
       end if;
     end if;
-  else
-    bit_cnt := 15;
-    miso <= 'Z';
-  end if;
-end process;
+  end process;
+
+  process (sclk_i, sel_i, ppm_i) is
+  variable bit_cnt: unsigned (3 downto 0) := (others => '1');
+  begin
+    if (sel_i = '0') then
+      if bit_cnt = 15 then
+        miso_o <= ppm_i;                         -- potentially useful to detect irq latency issues
+      else
+        miso_o <= output_register(to_integer(bit_cnt));
+      end if;
+      if (falling_edge(sclk_i)) then
+        if (bit_cnt = 0) then
+          bit_cnt := (others => '1');
+        else
+          bit_cnt := bit_cnt - 1;
+        end if;
+      end if;
+    else
+      bit_cnt := (others => '1');
+      miso_o <= 'Z';
+    end if;
+  end process;
+
+  process (irq_assert, sel_i, irq_deassert) is
+  begin
+    if rising_edge(irq_assert) then
+      ppm_irq_o <= sync;
+    end if;
+    if (sel_i = '0' or irq_deassert = '1') then
+      ppm_irq_o <= '0';
+    end if;
+  end process;
 
 end Behavioral;
--- src/gz_ppm/gz_ppm_testbench.vhd
+++ src/gz_ppm/gz_ppm_testbench.vhd
@@ -1,70 +1,66 @@
 --------------------------------------------------------------------------------
--- Company: 
--- Engineer:
+-- Company:        Guzunty 
+-- Engineer:       CDM
 --
--- Create Date:   13:48:11 07/05/2013
--- Design Name:   
--- Module Name:   /media/sf_Projects/Raspberry Pi/Guzunty/SB/gz_ppm/gz_ppm_testbench.vhd
+-- Create Date:   09:29:33 01/21/2015
+-- Design Name:   gz_ppm
+-- Module Name:   gz_ppm_testbench
 -- Project Name:  gz_ppm
--- Target Device:  
--- Tool versions:  
--- Description:   
+-- Target Device: None
+-- Tool versions: ISE 14
+-- Description:   Tests behaviour of a PPM decoder.
 -- 
--- VHDL Test Bench Created by ISE for module: gz_ppm
+-- VHDL Test Bench Created by ISE for module: gz_ppm3
 -- 
 -- Dependencies:
 -- 
 -- Revision:
--- Revision 0.01 - File Created
+-- Revision 1.5
 -- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
 ENTITY gz_ppm_testbench IS
 END gz_ppm_testbench;
-
+ 
 ARCHITECTURE behavior OF gz_ppm_testbench IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT gz_ppm
     PORT(
-         ppm_in : IN  std_logic;
-			sync : OUT std_logic;
-         ppm_int_req : OUT  std_logic;
-         ppm_clk : IN  std_logic;
-         miso : OUT  std_logic;
-         sclk : IN  std_logic;
-         sel : IN  std_logic;
-		   pwms : out STD_LOGIC_VECTOR (7 downto 0)
-        );
+         ppm_i : IN  std_logic;
+         ppm_clk_i : IN  std_logic;
+         pwms_o : OUT  std_logic_vector(7 downto 0);
+         miso_o : OUT  std_logic;
+         sclk_i : IN  std_logic;
+         sel_i : IN  std_logic;
+			sync_o : OUT std_logic;
+         ppm_irq_o : out std_logic
+			);
     END COMPONENT;
     
 
    --Inputs
-   signal ppm_in : std_logic := '1';
+   signal ppm_in : std_logic := '0';
    signal ppm_clk : std_logic := '0';
    signal sclk : std_logic := '0';
    signal sel : std_logic := '1';
 
  	--Outputs
+   signal pwms : std_logic_vector(7 downto 0);
    signal ppm_int_req : std_logic;
    signal miso : std_logic;
 	signal sync : std_logic;
-	signal pwms : std_logic_vector(7 downto 0);
+
+   -- Test control
+	signal processor_response_enable : std_logic := '1';
 
    -- Clock period definitions
-   constant ppm_clk_period : time := 480 ns; -- ~ 1024 x 10^3 Hz
+   constant ppm_clk_period : time := 480 ns; -- ~ 2 MHz
    constant sclk_period : time := 62.5 ns;   -- 16 MHz
- 
+		
    procedure normal_frame(signal ppm_in: out std_logic; max: time; min: time) is
 	variable center: time := ((max - min) / 2) + min ;
 	constant w: time := 0.3 ms; -- ppm pulse width
@@ -114,18 +110,19 @@ ARCHITECTURE behavior OF gz_ppm_testbench IS
 		ppm_in <= '1';
 	end normal_frame;
 
+
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: gz_ppm PORT MAP (
-          ppm_in => ppm_in,
-			 sync => sync,
-          ppm_int_req => ppm_int_req,
-          ppm_clk => ppm_clk,
-          miso => miso,
-          sclk => sclk,
-          sel => sel,
-			 pwms => pwms
+          ppm_i => ppm_in,
+          ppm_clk_i => ppm_clk,
+          pwms_o => pwms,
+          miso_o => miso,
+          sclk_i => sclk,
+          sel_i => sel,
+			 sync_o => sync,
+			 ppm_irq_o => ppm_int_req
         );
 
    -- Clock process definitions
@@ -143,7 +140,7 @@ BEGIN
 		wait for sclk_period/2;
 		sclk <= '1';
 		wait for sclk_period/2;
-   end process; 
+   end process;
 
    -- Stimulus process
    stim_proc: process
@@ -175,22 +172,35 @@ BEGIN
 		wait for 2 ms;                        -- 3rd Fr Sky case
 		normal_frame(ppm_in, 2 ms, 2 ms);     -- all maximum frame
 		wait for 8 ms;
+		normal_frame(ppm_in, 2 ms, 2 ms);     -- check recovery
+		wait for 8 ms;
+		processor_response_enable <= '0';     -- check no response from processor
+		normal_frame(ppm_in, 2 ms, 2 ms);
+		wait for 8 ms;
+		normal_frame(ppm_in, 2 ms, 2 ms);
+		wait for 8 ms;
+		processor_response_enable <= '1';
+		normal_frame(ppm_in, 2 ms, 2 ms);     -- check recovery
+		wait for 8 ms;
+		normal_frame(ppm_in, 2 ms, 2 ms);
+		wait for 8 ms;
       wait;
    end process;
-	
-	spi_proc: process (ppm_int_req, sclk) is
-	variable bit_count: natural range 0 to 15 := 0;
-	begin
-	  if (rising_edge(ppm_int_req)) then
-		 sel <= '0' after 50 us; -- simulate interrupt latency
-	  end if;
-	  if (rising_edge(sclk) and sel = '0') then
-	    if (bit_count /= 15) then
-		   bit_count := bit_count + 1;
-		 else
-		   bit_count := 0;
-		   sel <= '1';-- SPI transfer complete
-		 end if;
-	  end if;
-	end process;
+
+   spi_proc: process (ppm_int_req, sclk) is
+   variable bit_count: natural range 0 to 15 := 0;
+   begin
+     if (rising_edge(ppm_int_req) and processor_response_enable = '1') then
+   	 sel <= '0' after 50 us; -- simulate interrupt latency
+     end if;
+     if (rising_edge(sclk) and sel = '0') then
+       if (bit_count /= 15) then
+   	   bit_count := bit_count + 1;
+   	 else
+   	   bit_count := 0;
+   	   sel <= '1';-- SPI transfer complete
+   	 end if;
+     end if;
+   end process;
+
 END;
